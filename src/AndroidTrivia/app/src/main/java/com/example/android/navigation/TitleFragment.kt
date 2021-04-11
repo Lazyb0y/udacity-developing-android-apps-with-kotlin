5f958856ba11ae61b2508cee9b3b64c16e2d6d74
Foysal Ahamed <thisisfoysal@gmail.com>
2021-04-11 17:11:38 +0600

    Implemented safe arguments, code refactored

--- src/AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ src/AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -15,7 +15,9 @@ class TitleFragment : Fragment() {
                 inflater, R.layout.fragment_title, container, false
         )
 
-        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        binding.playButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
 
         setHasOptionsMenu(true)
 
